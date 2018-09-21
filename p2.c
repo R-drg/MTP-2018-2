1e48bac2fa5d114d616cc7b2709c623b12d50366
r-drg <rodrigohferreira210@gmail.com>
2018-09-21 15:27:51 -0300

    Add p2.c Menu

---
+++ p2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+int opt;
+
+while(opt!=9){
+printf("Escolha uma opcao no menu\n----------------------------------\n1. Binario para Decimal\n2. Binario para Hexadecimal\n3. Hexadecimal para Decimal\n4. Hexadecimal para Binario\n5. Decimal para Binario\n6. Decimal para Hexadecimal\n7. Octal para Hexadecimal\n8. Decimal para Octal \n9. EncerrarPrograma\n\n");
+printf("Sua escolha: ");
+scanf("%i",&opt);
+}
+}
